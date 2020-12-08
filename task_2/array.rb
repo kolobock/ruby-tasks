8fc08bd84e46b724c9c5f4bf245215f4e4a016a3
Andrii Rudenko <kolobock@gmail.com>
2020-12-08 18:27:57 +0200

    resolve task#2

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,19 @@
 class Array
+  def rebuild!(&block)
+    return self unless block_given?
+
+    self.map! do |element|
+      if element.is_a?(Array)
+        element.rebuild!(&block)
+      else
+        if block.arity == 1
+          block.call(element)
+        elsif block.arity == 2
+          block.call(element, self.size)
+        else
+          raise "#{__method__} does not support arity > 2! Feel free to extend it ;)"
+        end
+      end
+    end
+  end
 end
